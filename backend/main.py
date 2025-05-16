1588511bd5120ef3c59423a2f6bc87adeed38fee
michelledayana <102559251+michelledayana@users.noreply.github.com>
2025-05-15 20:25:32 -0500

    Update main.py
    parte3

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import uvicorn
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hola desde FastAPI en Railway"}
+
+# Configuración para Railway
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Usa el puerto de Railway o 8000 por defecto
+    uvicorn.run(app, host="0.0.0.0", port=port)
