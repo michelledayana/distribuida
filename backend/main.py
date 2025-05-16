69bbb86e82d71a29f7d2690d014d0799ac7f474b
michelledayana <102559251+michelledayana@users.noreply.github.com>
2025-05-15 20:30:07 -0500

    Update main.py
    parte5

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hola Mundo"}
+    return "message": "Hola Mundo"
 
 # Configuración para Railway
 if __name__ == "__main__":
